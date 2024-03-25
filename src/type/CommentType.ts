a1c3d94e6f3f1c0248545e4d2a0857a7fb70ac79
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-25 15:54:54 -0400

    Revert "Add the comment api"
    This reverts commit f10e32401e5410cdc12da1832a5ac8795c46072c.

--- src/type/CommentType.ts
+++
@@ -1,6 +0,0 @@
-export interface CommentType{
-    _id?: string,
-    targetUserID: string,
-    sourceUserID: string,
-    comment: string
-}
--- src/type/ProductType.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductType{
-    _id?: string,
-    image: string,
-    name: string,
-    description: string,
-    category: string,
-    price: number,
-    userID: string, 
-}
