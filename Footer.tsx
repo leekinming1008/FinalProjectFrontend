4367a14c1d13ab14a70d59b9273562c4418a6ee9
Jean Carlo <jean.carlo.latoure@gmail.com>
2024-03-26 17:46:57 -0400

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="Top-Footer">
+        <img src="../assets/Footer-Logos-1-Large.jpg" alt="" className="Footer-Logos-Large" />
+        <img src="../assets/Footer-Logos-1-Medium.jpg" alt="" className="Footer-Logos-Medium" />
+        <img src="../assets/Footer-Logos-1-Small.jpg" alt="" className="Footer-Logos-Small" />
+      </div>
+      <p><img src="../assets/Footer-Logos-2.jpg" alt="" className="Footer-Logos-2" /><br />© 2024 CantekShop.com. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
